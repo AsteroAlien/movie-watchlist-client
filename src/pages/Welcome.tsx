b74f347869f335dfd3249c731646efadfd052fc0
Charles Lemmert <charlesmoseslemmert@gmail.com>
2026-01-25 16:51:41 +0200

    Refactor theme variables and update Welcome page styles
    Moved CSS custom properties to a @theme block with hsl values and added new color tokens. Updated base layer styles for typography and layout. Refactored Welcome.tsx to use new Tailwind theme classes and improved section and footer styling for consistency.

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -2,16 +2,16 @@ import Box from '@mui/material/Box';
 import Header from './../components/Header';
 function Welcome() {
     return (
-        <div className='h-full bg-[hsl(var(--background))] film-grain'>
+        <div className='h-full bg-background film-grain'>
             <Header />
             <section id='welcome' className='h-full relative overflow-hidden'>
                 {/* Background glow */}
-                <Box className='absolute top-1/4 left-1/2 -translate-x-1/2 w-200 h-200 bg-[hsl(var(--primary))]/20 rounded-full blur-[150px] pointer-events-none'></Box>
+                <Box className='absolute top-1/4 left-1/2 -translate-x-1/2 w-200 h-200 bg-primary/20 rounded-full blur-[150px] pointer-events-none'></Box>
                 <Box className='container relative pt-20 pb-32'></Box>
             </section>
-            <section id='sub-info' className='h-[30rem] py24 bg-black film-grain'></section>
-            <section id='create-account' className='h-[30rem] py24 bg-[hsl(var(--background))] film-grain'></section>
-            <footer></footer>
+            <section id='sub-info' className='h-120 py24 bg-linear-to-b from-background to-secondary/20'></section>
+            <section id='create-account' className='h-120 py24 bg-background film-grain'></section>
+            <footer className="border-t border-border py-12"></footer>
         </div>
     )
 }
